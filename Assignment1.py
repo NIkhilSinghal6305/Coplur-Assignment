67a966174e5cd17654935fb83148fbc58d081442
Nikhil Singhal <nikhilagarwal6305@gmail.com>
2026-05-30 09:44:40 +0530

    Assignment

---
+++ Assignment1.py
@@ -0,0 +1,19 @@
+# 1. Practice print statement
+
+name = "Nikhil"
+age = 19
+print("Name:", name, "Age:", age)
+
+# Using separator
+print("Python", "NumPy", "Pandas", sep=" | ")
+
+# Using end
+print("Hello", end=" ")
+print("World")
+
+
+# 2. Ask user to enter name and display greeting
+
+user_name = input("\nEnter your name: ")
+
+print("Hello,", user_name + "! Welcome to Python Programming.")
